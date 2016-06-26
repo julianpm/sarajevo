8661f956808d2058871cec78eb30ac2c3d16aa0c
Julian Meanchoff <jpmeanchoff@gmail.com>
2016-06-26 19:24:18 -0400

    finished build out of sarajevo landing page

--- js/app.js
+++ js/app.js
@@ -1,12 +1,41 @@
 
 $(document).ready(function() {
 
+	// // FOUNDATION
 	$(document).foundation()
 
-	$('.work-pieces').isotope({
-	  // options
-	  itemSelector: '.work-pieces-single',
-	  layoutMode: 'fitRows'
-	});
+	// // ISOTOPE PLUGIN
+    var $container = $('.work-pieces');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false,
+        }
+    });
+  
+    $('.work-nav a').click(function(){
+      var selector = $(this).attr('data-filter');
+      $container.isotope({ 
+          filter: selector,
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false, 
+          }
+      });
+    return false;
+  });
 
 });
+
+// GOOGLE MAPS API
+var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 43.6309599, lng: -79.3580098},
+      zoom: 16,
+      scrollwheel:false
+    });
+  }
